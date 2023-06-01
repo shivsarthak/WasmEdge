37a0bd6372537ff3caac94fff4076dff3585086a
psainics <122770897+psainics@users.noreply.github.com>
2023-06-01 18:58:39 +0530

    WTF Fix https://github.com/microsoft/monaco-editor/issues/2122

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
+const prefix = `monaco-editor/esm/vs`;
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          jsonWorker: [`${prefix}/language/json/json.worker`],
+          cssWorker: [`${prefix}/language/css/css.worker`],
+          htmlWorker: [`${prefix}/language/html/html.worker`],
+          tsWorker: [`${prefix}/language/typescript/ts.worker`],
+          editorWorker: [`${prefix}/editor/editor.worker`],
+        },
+      },
+    },
+  },
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
