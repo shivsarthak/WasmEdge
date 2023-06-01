2149dd2ae6beb9738315cc5ff4d4279fda9d9f09
psainics <122770897+psainics@users.noreply.github.com>
2023-06-01 18:45:17 +0530

    Improve import

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
-import './style.css'
-import { WebContainer } from '@webcontainer/api';
-import { files } from './files';
-import { getEditor } from './monacoapi';
+import "./style.css";
+import { WebContainer } from "@webcontainer/api";
+import { files } from "./files";
+import { getEditor } from "./monacoapi";
+import * as monaco from "monaco-editor";
 
 /** @type {import('@webcontainer/api').WebContainer}  */
 let webcontainerInstance;
-let editor = null;
-window.addEventListener('load', async () => {
 
+/** @type {monaco.editor | null}  */
+let editor = null;
+
+window.addEventListener("load", async () => {
   // Get editor will init the editor if it's not initialized yet
   editor = getEditor();
   // Set the editor value to the contents of index.js
-  editor.getModel().setValue(files['index.js'].file.contents);
+  editor.getModel().setValue(files["index.js"].file.contents);  
   // Write the contents of the editor to index.js
   editor.getModel().onDidChangeContent((event) => {
     writeIndexJS(editor.getModel().getValue());
-	});
+  });
 
   // Call only once
   webcontainerInstance = await WebContainer.boot();
@@ -23,30 +26,32 @@ window.addEventListener('load', async () => {
 
   const exitCode = await installDependencies();
   if (exitCode !== 0) {
-    throw new Error('Installation failed');
-  };
+    throw new Error("Installation failed");
+  }
 
   startDevServer();
 });
 
 async function installDependencies() {
   // Install dependencies
-  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
-  installProcess.output.pipeTo(new WritableStream({
-    write(data) {
-      console.log(data);
-    }
-  }))
+  const installProcess = await webcontainerInstance.spawn("npm", ["install"]);
+  installProcess.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        console.log(data);
+      },
+    })
+  );
   // Wait for install command to exit
   return installProcess.exit;
 }
 
 async function startDevServer() {
   // Run `npm run start` to start the Express app
-  await webcontainerInstance.spawn('npm', ['run', 'start']);
+  await webcontainerInstance.spawn("npm", ["run", "start"]);
 
   // Wait for `server-ready` event
-  webcontainerInstance.on('server-ready', (port, url) => {
+  webcontainerInstance.on("server-ready", (port, url) => {
     iframeEl.src = url;
   });
 }
@@ -56,12 +61,11 @@ async function startDevServer() {
  */
 
 async function writeIndexJS(content) {
-  await webcontainerInstance.fs.writeFile('/index.js', content);
+  await webcontainerInstance.fs.writeFile("/index.js", content);
 }
 
-
 /** @type {HTMLIFrameElement | null} */
-const iframeEl = document.querySelector('iframe');
+const iframeEl = document.querySelector("iframe");
 
 /** @type {HTMLTextAreaElement | null} */
-const textareaEl = document.querySelector('textarea');
+const textareaEl = document.querySelector("textarea");
--- src/monacoapi.js
+++ src/monacoapi.js
@@ -1,3 +1,5 @@
+import * as monaco from "monaco-editor";
+
 let editor = null;
 
 const getEditor = () => {
