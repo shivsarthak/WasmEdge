673c4db8ff3e6d18c3331cbea7202a1511efd538
Shiv-Desktop <shivsarthak34@gmail.com>
2023-06-05 17:07:16 +0530

    build error fix

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ const prefix = `monaco-editor/esm/vs`;
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          jsonWorker: [`${prefix}/language/json/json.worker`],
-          cssWorker: [`${prefix}/language/css/css.worker`],
-          htmlWorker: [`${prefix}/language/html/html.worker`],
-          tsWorker: [`${prefix}/language/typescript/ts.worker`],
-          editorWorker: [`${prefix}/editor/editor.worker`],
-        },
-      },
-    },
   },
   server: {
     headers: {
