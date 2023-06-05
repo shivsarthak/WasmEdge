1c3dd2a45e0b70fe0e21179cea16b10e2d25c9e7
Shiv-Desktop <shivsarthak34@gmail.com>
2023-06-05 19:06:56 +0530

    build fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 const prefix = `monaco-editor/esm/vs`;
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        loading: resolve(__dirname, 'loading.html'),
+        complete: resolve(__dirname, 'complete.html'),
+      },
+    },
   },
   server: {
     headers: {
